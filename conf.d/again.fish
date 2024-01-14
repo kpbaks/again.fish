3253e1619e93e0490936176ce2a7fe34706d74d1
kpbaks <kristoffer.pbs@tuta.io>
2024-01-14 14:45:09 +0100

    feat: add code for `--mode=dynamic`, but do not enable it

--- conf.d/again.fish
+++ conf.d/again.fish
@@ -3,18 +3,23 @@ function _again_install --on-event again_install
 end
 
 function _again_update --on-event again_update
-	# Migrate resources, print warnings, and other update logic.
+    # Migrate resources, print warnings, and other update logic.
 end
 
 function _again_uninstall --on-event again_uninstall
-	# Erase "private" functions, variables, bindings, and other uninstall logic.
+    # Erase "private" functions, variables, bindings, and other uninstall logic.
 end
 
 
-set --query AGAIN_KEYBIND; or set --global AGAIN_KEYBIND \ea
-set --query AGAIN_ENABLED_MESSAGE; or set --global --export "again enabled ($AGAIN_KEYBIND to disable) "
-# set --query AGAIN_ENABLED_MESSAGE; or set --global --export "again enabled (alt+a to disable) "
+set --query AGAIN_KEYBIND; or set --global AGAIN_KEYBIND \ea # alt+a
+# set --query AGAIN_ENABLED_MESSAGE; or set --global --export AGAIN_ENABLED_MESSAGE "again enabled ($AGAIN_KEYBIND to disable) "
+set --query AGAIN_ENABLED_MESSAGE; or set --global --export AGAIN_ENABLED_MESSAGE "again enabled (alt+a to disable) "
+# set --query AGAIN_DYNAMIC_KEYBIND; or set --global AGAIN_DYNAMIC_KEYBIND \eA # alt+A
+# set --query AGAIN_DYNAMIC_ENABLED_MESSAGE; or set --global --export AGAIN_DYNAMIC_ENABLED_MESSAGE "again-dynamic enabled ($AGAIN_DYNAMIC_KEYBIND to disable) "
+# set --query AGAIN_DYNAMIC_ENABLED_MESSAGE; or set --global --export AGAIN_DYNAMIC_ENABLED_MESSAGE "again-dynamic enabled (alt+A to disable) "
 
-# alt+a
 # bind \ea 'again'
-bind $AGAIN_KEYBIND 'again'
+bind $AGAIN_KEYBIND 'again --mode=static'
+
+# bind \eA 'again dynamic'
+# bind $AGAIN_DYNAMIC_KEYBIND 'again --mode=dynamic'
--- functions/again.fish
+++ functions/again.fish
@@ -1,5 +1,5 @@
 function again -d "After executing a command, put it back in the command line"
-    set -l options h/help
+    set -l options h/help (fish_opt --short=m --long=mode --required-val)
     if not argparse $options -- $argv
         eval (status function) --help
         return 2
@@ -7,13 +7,14 @@ function again -d "After executing a command, put it back in the command line"
 
     if set --query _flag_help
         set -l reset (set_color normal)
-        set -l bold (set_color bold)
+        set -l bold (set_color --bold)
         set -l red (set_color red)
         set -l green (set_color green)
         set -l yellow (set_color yellow)
         printf "%sAfter executing a command, put it back in the command line%s\n" $bold $reset >&2
         printf "\n" >&2
         printf "%sUSAGE:%s %s%s%s [enabled]\n" $yellow $reset (set_color $fish_color_command) (status function) $reset >&2
+        # TODO: mention flags
         return 0
     end
 
@@ -25,7 +26,7 @@ function again -d "After executing a command, put it back in the command line"
     if test (count $argv) -eq 1
         switch $argv[1]
             case enabled
-                set --query AGAIN_ENABLED
+                set --query AGAIN_ENABLED; or set --query AGAIN_DYNAMIC_ENABLED
                 return $status
             case '*'
                 printf "%serror%s: invalid argument '%s'\n" (set_color red) (set_color normal) $argv[1] >&2
@@ -34,29 +35,71 @@ function again -d "After executing a command, put it back in the command line"
         end
     end
 
-    set -l buffer (commandline)
-    set -l cursor (commandline --cursor)
+    if not set --query _flag_mode
+        set -l reset (set_color normal)
+        printf "%serror%s: %s-m%s, %s--mode%s MODE not specified\n" (set_color red) $reset (set_color $fish_color_param) $reset (set_color $fish_color_param) $reset
+        return 2
+    else
+        if not contains -- $_flag_mode static dynamic
+            set -l reset (set_color normal)
+            printf "%serror%s: unknown mode: %s%s%s. valid modes are: %s%s%s\n" (set_color red) $reset (set_color red) $_flag_mode $reset (set_color --bold) (string join ", " static dynamic) $reset
+            return 2
+        end
+    end
 
     if set --query AGAIN_ENABLED
+        # Disable (static) again mode
         functions --query again_postexec_handler; and functions --erase again_postexec_handler
         set --unexport AGAIN_ENABLED
         set --erase AGAIN_ENABLED
-    else if string match --regex --quiet '^\s*$' $buffer
-        # Do nothing
-    else
-        set -g again_command $buffer
-        set -g again_cursor $cursor
-        function again_postexec_handler --on-event fish_postexec
-            commandline --replace $again_command
-            commandline --cursor $again_cursor
-        end
+        set --function again_disabled_this_function_call
 
-        # Even though the value of the variable is not used, in this function
-        # we are setting it, such that starship.rs (or other prompt) can use it.
-        # set --global --export AGAIN_ENABLED "again enabled (alt+a to disable) "
-        set --global --export AGAIN_ENABLED $AGAIN_ENABLED_MESSAGE
-
+    else if set --query AGAIN_DYNAMIC_ENABLED
+        # Disable (dynamic) again
+        functions --query again_postexec_handler; and functions --erase again_postexec_handler
+        functions --query again_preexec_handler; and functions --erase again_preexec_handler
+        set --unexport AGAIN_DYNAMIC_ENABLED
+        set --erase AGAIN_DYNAMIC_ENABLED
+        set --function again_dynamic_disabled_this_function_call
     end
 
+    if string match --regex --quiet '^\s*$' $buffer
+        # Do nothing
+        return
+    end
+
+    switch $_flag_mode
+        case static
+            if not set --query again_disabled_this_function_call
+                set -g again_command (commandline)
+                set -g again_cursor (commandline --cursor)
+                function again_postexec_handler --on-event fish_postexec
+                    commandline --replace $again_command
+                    commandline --cursor $again_cursor
+                end
+
+                # Even though the value of the variable is not used, in this function
+                # we are setting it, such that starship.rs (or other prompt) can use it.
+                # set --global --export AGAIN_ENABLED "again enabled (alt+a to disable) "
+                set --global --export AGAIN_ENABLED $AGAIN_ENABLED_MESSAGE
+            end
+        case dynamic
+            if not set --query again_dynamic_disabled_this_function_call
+                set -g again_command (commandline)
+                set -g again_cursor (commanline --cursor)
+                function again_postexec_handler --on-event fish_postexec
+                    commandline --replace $again_command
+                    commandline --cursor $again_cursor
+                end
+                # FIX: does not work, maybe have to fix in fish-shell source
+                function again_preexec_handler --on-event fish_preexec
+                    set -g again_command (commandline)
+                    set -g again_cursor (commandline --cursor)
+                end
+                # Even though the value of the variable is not used, in this function
+                # we are setting it, such that starship.rs (or other prompt) can use it.
+                set --global --export AGAIN_DYNAMIC_ENABLED $AGAIN_DYNAMIC_ENABLED_MESSAGE
+            end
+    end
     commandline --function repaint
 end
